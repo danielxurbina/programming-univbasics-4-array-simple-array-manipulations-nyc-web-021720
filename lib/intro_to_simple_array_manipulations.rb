0ba79ae9374763c180e65bc51231d9177d878c55
Daniel Urbina <danielur1999@gmail.com>
2020-02-06 03:42:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,7 +22,7 @@ end
 
 def using_shift(array)
   array = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-  im_so_over_this_city = array.shift(array)
+  im_so_over_this_city = array.shift
 end
 
 def shift_with_args(array)

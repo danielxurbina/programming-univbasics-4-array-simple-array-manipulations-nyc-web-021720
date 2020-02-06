aee2532bae6be606083499cd9675058501b3b7ba
Daniel Urbina <danielur1999@gmail.com>
2020-02-06 03:01:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -57,7 +57,7 @@ end
 
 def using_delete(array, element)
   array = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-  no_offense_steven = array.delete(1)
+  no_offense_steven = array.delete(array, 1)
 end
 
 def using_delete_at(array, element)
